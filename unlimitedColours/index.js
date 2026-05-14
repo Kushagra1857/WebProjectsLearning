72dc536ec8e56e1d6bb3a4fd34c487a96b4fc7f5
Kushagra1857 <kushgara1857@gmail.com>
2026-05-14 19:00:54 +0530

    added new mini project

---
+++ unlimitedColours/index.js
@@ -0,0 +1,30 @@
+console.log("this is a check files")
+
+const startButton = document.querySelector('#start')
+const stopButton = document.querySelector('#stop')
+const body = document.querySelector('body')
+
+const randomColour = function(){
+    let hex = '0123456789ABCDEF'
+    let color = '#'
+    for(let i=0; i<6; i++){
+        color += hex[Math.floor(Math.random()*16)]
+    }
+    return color
+}
+let changingColours
+const colourStart = function() {
+    const changeBackgroyundColour = function() {
+        let colourThisTime =  randomColour()
+        body.style.backgroundColor = colourThisTime
+        // console.log(colourThisTime);
+    }
+    if(!changingColours) changingColours = setInterval(changeBackgroyundColour, 1000);
+}
+const stopColourChange = function(){
+    clearInterval(changingColours)
+    changingColours = null
+}
+
+startButton.addEventListener('click', colourStart)
+stopButton.addEventListener('click', stopColourChange)
